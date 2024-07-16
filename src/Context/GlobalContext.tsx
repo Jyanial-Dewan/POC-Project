59656b6148f8ec2aa10945d02f2afbec128a8971
n00bNn00b <dewanreal@gmail.com>
2024-07-16 22:50:01 +0600

    infinite loop fixed

--- src/Context/GlobalContext.tsx
+++ src/Context/GlobalContext.tsx
@@ -1,16 +1,22 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import axios from "axios";
 
 interface GlobalContextProviderProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 interface Persons {
-    user_id?: number;
-    first_name?: string;
-    middle_name?: string;
-    last_name?: string;
-    job_title?: string; 
+  user_id?: number;
+  first_name?: string;
+  middle_name?: string;
+  last_name?: string;
+  job_title?: string;
 }
 
 // interface Token {
@@ -21,41 +27,44 @@ interface Persons {
 // }
 
 interface GlobalContex {
-    open: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-    token: string;
-    setToken: React.Dispatch<React.SetStateAction<string>>;
-    persons: Persons[];
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  token: string;
+  setToken: React.Dispatch<React.SetStateAction<string>>;
+  persons: Persons[];
 }
 
-const GlobalContex = createContext({} as GlobalContex)
+const GlobalContex = createContext({} as GlobalContex);
 
 export function useGlobalContext() {
-    return useContext(GlobalContex)
+  return useContext(GlobalContex);
 }
 
-export function GlobalContextProvider({children}: GlobalContextProviderProps) {
-    const [open, setOpen] = useState<boolean>(false);
-    const [token, setToken] = useState<string>('');
-    const [persons, setPersons] = useState<Persons[]>([]);
+export function GlobalContextProvider({
+  children,
+}: GlobalContextProviderProps) {
+  const [open, setOpen] = useState<boolean>(false);
+  const [token, setToken] = useState<string>("");
+  const [persons, setPersons] = useState<Persons[]>([]);
 
-    const fetchData = async () => {
-        try {
-          const response = await axios.get('/api/v2/def-persons');
-          console.log(response.data);
-          setPersons(response.data);
-        } catch (error) {
-          console.error('Error fetching data:', error); // Handle any errors
-        }
-      };
-      
-      fetchData();
-      console.log(persons);
-    
-    return (
-        <GlobalContex.Provider value={{open, setOpen, token, setToken, persons}}>
-            {children}
-        </GlobalContex.Provider>
-    )
-    
+  const fetchData = async () => {
+    await axios
+      .get("/api/v2/def-persons")
+      .then((res) => {
+        console.log(res.data);
+        setPersons(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(persons);
+
+  return (
+    <GlobalContex.Provider value={{ open, setOpen, token, setToken, persons }}>
+      {children}
+    </GlobalContex.Provider>
+  );
 }
